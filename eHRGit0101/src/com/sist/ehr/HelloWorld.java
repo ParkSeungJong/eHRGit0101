3c4e4a91d47bc18a600ff7c89e1b466d8d3744a8
sist <sist@DESKTOP-LM7N5HR>
2020-04-07 10:33:38 +0900

    Commit Test

---
+++ eHRGit0101/src/com/sist/ehr/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.sist.ehr;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Git.");
+		System.out.println("Hello, Git01.");
+		System.out.println("민지킴");
+
+	}
+
+}
---
+++ eHRGit0101/src/eHRGit0101/HelloWorld.java
@@ -0,0 +1,11 @@
+package eHRGit0101;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Git.");
+		System.out.println("Eclipse");
+
+	}
+
+}
