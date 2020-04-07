73f841a5e57956c37c27232edaafcb96f2a22d46
SIST <SIST@DESKTOP-5KKJ7F7>
2020-04-07 10:30:39 +0900

    민지킴

--- eHRGit0101/src/com/sist/ehr/HelloWorld.java
+++ eHRGit0101/src/com/sist/ehr/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, Git.");
 		System.out.println("Hello, Git01.");
+		System.out.println("민지킴");
 
 	}
 
